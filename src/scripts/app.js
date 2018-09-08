acd839c9b3a6754e74e1247d81d6d795c6947d3d
SkipTyler <DetyaJah@protonmail.com>
2018-09-08 18:32:18 +0300

    add hover button bg body

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// mobile menu
+
+(() => {
+
+	const menu = document.querySelector('.js-mobileMenu');
+	const menuBtn = document.querySelector('.js-btnMenu');
+
+	menuBtn.addEventListener('click', ev => {
+		const target = ev.target || ev.currentTarget;
+
+		if (target.classList.contains('active')) {
+			target.classList.remove('active');
+			menu.classList.remove('active');
+			target.classList.add('close');
+			setTimeout(() => {
+				target.classList.remove('close');
+			}, 500)
+		} else {
+			target.classList.add('active');
+			menu.classList.add('active');
+		}
+	})
+
+
+})();
