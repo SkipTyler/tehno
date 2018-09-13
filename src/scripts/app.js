8087a41cffd6e240d2e74e0a3b9b3ff413f09955
gats <brightimpala@gmail.com>
2018-09-13 19:33:21 +0300

    slider switching is ready

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -163,6 +163,21 @@ document.addEventListener('click', (e) => {
             }
         }
     }
+
+    // slider of navigation
+	else if (clickedElement.parentNode.classList.contains('nav-slider_nav')) {
+		const slides = document.querySelectorAll('.nav-slider_overlay_slide');
+
+		for (let slide of slides) {
+
+			if (slide.classList.contains('active')) {
+				slide.classList.remove('active');
+			}
+			else {
+				slide.classList.add('active')
+			}
+		}
+	}
 });
 
 // services card
